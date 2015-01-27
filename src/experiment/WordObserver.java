5ba802b88db26004ea18cb9c93348484405f71e6
Garrett Benoit <Garrett_Benoit@baylor.edu>
2015-01-26 19:21:07 -0600

    Adding WordObserver to the git index.

---
+++ src/experiment/WordObserver.java
@@ -0,0 +1,8 @@
+package experiment;
+
+import enums.Key;
+
+public interface WordObserver {
+    public void wordSetEventObserved(String word);
+    public void currentLetterIndexChangedEventObservered(int letterIndex, Key key);
+}
